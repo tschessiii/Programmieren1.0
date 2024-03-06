9387c54f40c88ca4a59c28e5ef7eca3b8917568a
s54825 <s54825@edu.campus02.at>
2024-03-06 14:50:17 +0100

    Commit

--- src/Palindrom.java
+++ src/Palindrom.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
+import java.util.SimpleTimeZone;
+
 public class Palindrom {
     public static void main(String[] args){
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Bitte geben Sie hier hier ein Wort ein:");
+        String eingabe = scanner.next();
+
+
 
     }
 }
