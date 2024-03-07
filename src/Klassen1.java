97cf8441ad037e6d49b6720291d3ab431ebc0a5a
Jessi <s54825@edu.campus02.at>
2024-03-07 13:19:28 +0100

    commit

---
+++ src/Klassen1.java
@@ -0,0 +1,31 @@
+public class Klassen1 {
+    public static void main(String[] args){
+
+       /*
+        int alter_bello =3;
+        String rasse_bello= "Meine Rasse ist: Dackel";
+        String name_bello="Bello";
+        String bellen = "Woof, ich bin "+name_bello;
+        String hupfen = "Hupf,Hupf";
+        */
+
+        Kreis k1 = new Kreis(3);
+        Kreis k2= new Kreis(2);
+        Kreis k3= new Kreis(6);
+
+        System.out.println(k1);
+        System.out.println(k2);
+        System.out.println(k3);
+
+
+        /*
+        System.out.println(k1.radius);
+        System.out.println(k1.getArea());
+        System.out.println();
+         */
+
+
+
+
+    }
+}
