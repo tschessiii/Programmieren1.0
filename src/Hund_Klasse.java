2d3c3505eb293efa48d96c948aa495dd723fde73
Jessi <s54825@edu.campus02.at>
2024-03-07 13:19:28 +0100

    commit

---
+++ src/Hund_Klasse.java
@@ -0,0 +1,19 @@
+public class Hund_Klasse {
+    private int alter;
+    private String rasse;
+    private String name;
+
+    public Hund_Klasse(int alter,int name,int rasse){
+
+    }
+
+    public String bellen(){
+        return "Woof, ich bin "+name;
+    }
+
+    public void springen(){
+        System.out.println("Hupf,Hupf");
+    }
+
+
+}
---
+++ src/Kreis.java
@@ -0,0 +1,19 @@
+public class Kreis {
+
+    public int radius;
+
+    //Constructor mit dem wir neue Kreise erstellen
+    public Kreis(int radius) {
+        this.radius = radius;
+    }
+
+    public double getArea(){
+        return Math.PI *radius*radius;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Radius: "+radius+", Fläche: "+getArea();
+    }
+}
