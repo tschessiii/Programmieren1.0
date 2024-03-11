68db386bafa3c2943bef709c7d5883f576eeb205
Jessi <s54825@edu.campus02.at>
2024-03-11 14:57:05 +0100

    Commit

--- src/Hausübung2/Messung.java
+++ src/Hausübung2/Messung.java
@@ -1,25 +1,22 @@
 package Hausübung2;
 
 public class Messung {
-    public double[][] messung;
-    public int woche;
+    private double[][] messungen;
+    private int woche;
 
-    public Messung(double[][] messung, int woche) {
-        this.messung = new double[7][3];
+    public Messung(int woche) {
         this.woche = woche;
+        this.messungen = new double[7][3];
 
         //Initalisieren des Arrays mit den Standart Werten 0.0
         for (int i = 0; i<7;i++){
             for(int j=0;j<3;j++){
-                this.messung[i][j]=0.0;
+                this.messungen[i][j]=0.0;
             }
         }
     }
 
-
-
-
-    public int Tageszahl(String tag){
+    private int Tageszahl(String tag){
         switch (tag.toLowerCase()){
             case "Montag":
                 return 0 ;
@@ -52,8 +49,8 @@ public class Messung {
         int tageszahl = Tageszahl(tag);
         if(tageszahl != -1){//gültiger Tag (Wenn tageszahl nicht gleich -1 ist, ist es ein gültiger Tag
             for(int i = 0; i<3; i++){
-                if(messung[tageszahl][i] == 0.0){//Freier Slot
-                    messung[tageszahl][i]= messwert;
+                if(messungen[tageszahl][i] == 0.0){//Freier Slot
+                    messungen[tageszahl][i]= messwert;
                     System.out.println("Messung am "+tag+" erfolgreich gespeichert!");
                    return;
                 }
