a1ae9295e06de745ce813955dbae9ed50c1cf3d3
Jessi <s54825@edu.campus02.at>
2024-03-19 11:32:19 +0100

    Commit

---
+++ src/MatheRechner.java
@@ -0,0 +1,19 @@
+public class MatheRechner {
+
+    public static void add(int a, int b){
+        System.out.println(a+b);
+    }
+
+    public static void sub(int a, int b){
+        System.out.println(a-b);
+    }
+
+    public static void mul(int a, int b){
+        System.out.println(a*b);
+    }
+    public static void div(int a,int b){
+        System.out.println(a/b);
+    }
+
+
+}
