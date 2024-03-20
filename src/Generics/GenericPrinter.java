2eee1d7fbabeed73f8ad01fccd384385cf7c9d44
Jessi <s54825@edu.campus02.at>
2024-03-20 10:34:17 +0100

    Commit

---
+++ src/Generics/GenericPrinter.java
@@ -0,0 +1,9 @@
+package Generics;
+
+//großes T zeigt Java das es ein generics ist
+public class GenericPrinter<T> {
+    public void print(T thingToPrint){
+        System.out.println(thingToPrint);
+
+    }
+}
